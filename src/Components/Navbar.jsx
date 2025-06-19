770f58c7bdb047236c126921b742ced2be36b5f0
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-06-19 13:38:56 -0400

    nav name

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <a className="btn btn-ghost text-xl">daisyUI</a>
+      <a className="btn btn-ghost text-xl">Iris-Based-Detection</a>
     </div>
   )
 }
