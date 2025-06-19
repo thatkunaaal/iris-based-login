8060bf563a22e0581f805eaa0d7da2b2982c750a
thatkunaaal <thatkunaal@gmail.com>
2025-06-19 22:37:08 +0530

    npm i tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Body from './Components/Body'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Body/>
     </>
   )
 }
---
+++ src/Components/Body.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Body = () => {
+  return (
+    <div>
+      <figure className="diff aspect-16/9" tabIndex={0}>
+        <div className="diff-item-1" role="img" tabIndex={0}>
+          <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+            DAISY
+          </div>
+        </div>
+        <div className="diff-item-2" role="img">
+          <div className="bg-base-200 grid place-content-center text-9xl font-black">
+            DAISY
+          </div>
+        </div>
+        <div className="diff-resizer"></div>
+      </figure>
+    </div>
+  );
+};
+
+export default Body;
