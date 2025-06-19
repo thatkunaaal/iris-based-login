a45a2c5448b8df7be6f91f8db6a7e39066b746dd
thatkunaaal <thatkunaal@gmail.com>
2025-06-20 00:06:33 +0530

    sign-up card done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,54 +1,148 @@
 import React from "react";
+import { useState } from "react";
 
 const Body = () => {
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    <div className="flex grow justify-center items-center">
-    <div className="w-96 m-10">
-      <div className="card w-96 bg-base-100 card-xl shadow-sm">
-        <div className="card-body">
-          <h2 className="card-title ">Login to X</h2>
-          <label className="input validator m-1">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </g>
-            </svg>
-            <input
-              type="text"
-              required
-              placeholder="Username"
-              pattern="[A-Za-z][A-Za-z0-9\-]*"
-              minlength="3"
-              maxlength="30"
-              title="Only letters, numbers or dash"
-            />
-          </label>
-          <div className="justify-start  card-actions">
-            <button class="btn btn-neutral m-1"> Login with Eye</button>
-          </div>
-          <div className="flex justify-center">
-            <span>New user? Signup here</span>
-          </div>
-           
+    <div className="flex mt-10  justify-center items-center">
+      <div className="w-96 m-10">
+        <div className="card w-96 bg-base-100 card-xl shadow-sm">
+          {isLogin ? (
+            <div className="card-body">
+              <h2 className="card-title ">Login to X</h2>
+              <label className="input validator m-1">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  required
+                  placeholder="Username"
+                  pattern="[A-Za-z][A-Za-z0-9\-]*"
+                  minlength="3"
+                  maxlength="30"
+                  title="Only letters, numbers or dash"
+                />
+              </label>
+              <div className="justify-start  card-actions">
+                <button class="btn btn-neutral m-1"> Login with Eye</button>
+              </div>
+              <div className="flex justify-center">
+                <span
+                  className="w-xs text-center underline cursor-pointer underline-offset-2"
+                  onClick={() => {
+                    setIsLogin(!isLogin);
+                  }}
+                >
+                  {isLogin
+                    ? "New user? Signup here"
+                    : "Exisiting user, login here"}
+                </span>
+              </div>
+            </div>
+          ) : (
+            <div className="card-body">
+              <h2 className="card-title ">Signup to X</h2>
+              <label className="input validator m-1">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  required
+                  placeholder="Username"
+                  pattern="[A-Za-z][A-Za-z0-9\-]*"
+                  minlength="3"
+                  maxlength="30"
+                  title="Only letters, numbers or dash"
+                />
+              </label>
+              <label className="input validator m-1">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                >
+                  <g fill="none">
+                    <path
+                      d="M7.25 11.5C6.83579 11.5 6.5 11.8358 6.5 12.25C6.5 12.6642 6.83579 13 7.25 13H8.75C9.16421 13 9.5 12.6642 9.5 12.25C9.5 11.8358 9.16421 11.5 8.75 11.5H7.25Z"
+                      fill="currentColor"
+                    ></path>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M6 1C4.61929 1 3.5 2.11929 3.5 3.5V12.5C3.5 13.8807 4.61929 15 6 15H10C11.3807 15 12.5 13.8807 12.5 12.5V3.5C12.5 2.11929 11.3807 1 10 1H6ZM10 2.5H9.5V3C9.5 3.27614 9.27614 3.5 9 3.5H7C6.72386 3.5 6.5 3.27614 6.5 3V2.5H6C5.44771 2.5 5 2.94772 5 3.5V12.5C5 13.0523 5.44772 13.5 6 13.5H10C10.5523 13.5 11 13.0523 11 12.5V3.5C11 2.94772 10.5523 2.5 10 2.5Z"
+                      fill="currentColor"
+                    ></path>
+                  </g>
+                </svg>
+                <input
+                  type="tel"
+                  className="tabular-nums"
+                  required
+                  placeholder="Phone"
+                  pattern="[0-9]*"
+                  minlength="10"
+                  maxlength="10"
+                  title="Must be 10 digits"
+                />
+              </label>
+              <div className="grow justify-center  card-actions">
+                <button class="btn btn-neutral w-full ml-1 mt-1 mb-1"> 
+                   <svg viewBox="0 0 24 24" className="w-6" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000">
+                   <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                   <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                   <g id="SVGRepo_iconCarrier"> 
+                    <path d="M12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+                    <path d="M3 16.8V9.2C3 8.0799 3 7.51984 3.21799 7.09202C3.40973 6.71569 3.71569 6.40973 4.09202 6.21799C4.51984 6 5.0799 6 6.2 6H7.25464C7.37758 6 7.43905 6 7.49576 5.9935C7.79166 5.95961 8.05705 5.79559 8.21969 5.54609C8.25086 5.49827 8.27836 5.44328 8.33333 5.33333C8.44329 5.11342 8.49827 5.00346 8.56062 4.90782C8.8859 4.40882 9.41668 4.08078 10.0085 4.01299C10.1219 4 10.2448 4 10.4907 4H13.5093C13.7552 4 13.8781 4 13.9915 4.01299C14.5833 4.08078 15.1141 4.40882 15.4394 4.90782C15.5017 5.00345 15.5567 5.11345 15.6667 5.33333C15.7216 5.44329 15.7491 5.49827 15.7803 5.54609C15.943 5.79559 16.2083 5.95961 16.5042 5.9935C16.561 6 16.6224 6 16.7454 6H17.8C18.9201 6 19.4802 6 19.908 6.21799C20.2843 6.40973 20.5903 6.71569 20.782 7.09202C21 7.51984 21 8.0799 21 9.2V16.8C21 17.9201 21 18.4802 20.782 18.908C20.5903 19.2843 20.2843 19.5903 19.908 19.782C19.4802 20 18.9201 20 17.8 20H6.2C5.0799 20 4.51984 20 4.09202 19.782C3.71569 19.5903 3.40973 19.2843 3.21799 18.908C3 18.4802 3 17.9201 3 16.8Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+                    </g></svg> 
+                    Open Camera</button>
+              </div>
+              <div className="justify-start  card-actions">
+                <button class="btn btn-neutral m-1"> Login with Eye</button>
+              </div>
+              <div className="flex justify-center">
+                <span
+                  className="w-xs text-center underline cursor-pointer underline-offset-2"
+                  onClick={() => {
+                    setIsLogin(!isLogin);
+                  }}
+                >
+                  {isLogin
+                    ? "New user? Signup here"
+                    : "Exisiting user, login here"}
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      {/* <p className="validator-hint">
-            Must be 3 to 30 characters
-            <br />
-            containing only letters, numbers or dash
-          </p> */}
-    </div>
     </div>
   );
 };
